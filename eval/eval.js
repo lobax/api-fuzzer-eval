4ead4f6f59114f7cc6d175e22f3f986fae219dec
Kristian Alvarez Jörgensen <krijor@kth.se>
2019-12-02 19:03:07 +0100

    Update according to new changes to APIFuzzer

--- eval/eval.js
+++ eval/eval.js
@@ -1,18 +1,20 @@
 
 module.exports = function eval(results, dict) {
+    let matches = 0;
+    let bugs_found = 0;
     for (const error of results) {
         for (const bug of dict) {
             const url = error.request_url;
             const method = error.request_method;
-            const body = error.request_body;
+            const body = error.request_body ? error.request_body : "";
 
             const urlMatch = url.match(bug.path);
             const methodMatch = method.match(bug.method);
-            const bodyMatch = method.match(bug.body);
+            const bodyMatch = body.match(bug.body);
 
             if (urlMatch && methodMatch && bodyMatch) {
                 bug.match = true;
-                console.log(`Test no ${error.test_number} matches bug no ${bug.id}`);
+                matches += 1;
             }
         }
     }
@@ -20,6 +22,11 @@ module.exports = function eval(results, dict) {
     for (bug of dict) {
         if (bug.match) {
             console.log(`Bug no ${bug.id} found: ${bug.name}`)
+            bugs_found += 1;
         }
     }
+    console.log("\nRESULTS:");
+    console.log(`Bugs found: ${bugs_found}`);
+    console.log(`Total reports matched with bugs: ${matches}`);
+    console.log(`Uncataloged reports: ${results.length - matches}`);
 }
--- eval/parse.js
+++ eval/parse.js
@@ -6,7 +6,9 @@ const files = process.argv.splice(process.execArgv.length + 2);
 console.log(`Number of files: ${files.length}`);
 const results = files.map(file =>
     fs.readFile(file)
-        .then(data => JSON.parse(data))
+        .then(data =>
+            data.length ? JSON.parse(data) : null
+        )
 );
 
 
@@ -14,29 +16,19 @@ const dict = [
     {
         id: 0,
         name: 'Type Error',
-        path: /exception\/\d*[^\d\s]+\d*$/,
+        path: /exception\/.*[^\d\s]+.*$/,
+        method: /get/i,
+        body: /[\s\S]*/
+    },
+    {
+        id: 1,
+        name: 'Type Error',
+        path: /exception\/.*[^\d\s]+.*$/,
         method: /get/i,
         body: /[\s\S]*/
     },
 ]
 
 Promise.all(results)
-    .then(results => results.map(res => decode(res)))
+    .then(results => results.filter(obj => obj ? true : false))
     .then(results => eval(results, dict))
-
-function decode(result) {
-    return {
-        ...result,
-        request_url: decodeStr(result.request_url),
-        request_method: decodeStr(result.request_method),
-        request_body: decodeStr(result.request_body),
-    }
-}
-
-function decodeStr(str) {
-    if (str)
-        return Buffer.from(str, 'base64').toString('utf8')
-    else
-        return ""
-}
-
